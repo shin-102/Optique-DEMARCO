5c7632a19d58ab06d677cd3f8cf3ad375b22161e
shin-102 <hydro.wild@gmail.com>
2024-09-21 18:40:15 +0100

    website complete v1

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
   theme: {
      fontFamily: {
         optima: ['Optima', 'sans-serif'],
+        arsenal: ['Arsenal', 'sans-serif'],
       },
       extend: {
         colors: {
